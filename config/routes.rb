eab2235b8191b5ae20bbae96bf72f52ca59a376f
Damien Milon <milon.damien@gmail.com>
2020-02-11 20:22:24 +0100

    Cleaned git status

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # As a user, I can create a new pet
+  get "pets/new", to: 'pets#new', as: :new_pet
+  post "pets", to: 'pets#create'
+
+  # As a user, I can list all pets
+  get "pets", to: 'pets#index'
+  # As a user, I can see details of a pet
+  get "pets/:id", to: 'pets#show', as: :pet
+  # As a user, I can edit an existing pet
+  get "pets/:id/edit", to: 'pets#edit', as: :edit_pet
+  patch "pets/:id", to: 'pets#update'
+  # As a user, I can delete a pet
+  delete "pets/:id", to: 'pets#toto'
+  # resources :pets
 end
